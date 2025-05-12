205ae76e61639b709cd9bc60d2f9b72f96a5ca91
Gabriel-Mori <gabrielmmori51@outlook.com>
2025-05-11 21:33:28 -0300

    trocando a url de inicio

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       if (user) {
         router.push("/dashboard");
       } else {
-        router.push("/login");
+        router.push("/");
       }
     }
   }, [user, isLoading, router]);
